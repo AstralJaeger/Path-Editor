c85513d55c60bda7ecc408626010ba6fa3b39138
Felix Hillebrand <97hilfel@gmail.com>
2021-11-15 19:39:09 +0100

    Implement, remove, up, down, remove duplicates and dead path functions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import ctypes
+import os
 import sys
 import subprocess
 import tkinter as tk
+import tkinter.ttk
+import collections
 
 from typing import List, Union, Tuple
 from os import path, listdir
@@ -10,23 +13,25 @@ from tkinter import *
 from tkinter import ttk
 
 debug = True
+applications: List[str] = []
+treeview: ttk.Treeview
+duplicates_label: Label
+total_entries_label: Label
+total_length_label: Label
 
 
 def main():
     # create window
     main_window = create_window()
-    # print(f'Querying path from OS')
-    applications = get_path_from_os()
 
-    create_widgets(main_window, applications)
+    global applications
+    applications = get_path_from_os()
+    create_widgets(main_window)
 
     not_found = 0
     for i, application in enumerate(applications):
         exists = path.exists(application)
         not_found += (1 if not exists else 0)
-        # print(f'{i}. {application} - {"exists" if exists else "not found"}')
-
-    # print(f'Unused entries: {not_found}')
 
     # Start tk loop
     main_window.mainloop()
@@ -37,28 +42,111 @@ def create_window() -> Tk:
     return Tk()
 
 
-def create_widgets(window: Tk, applications) -> None:
-    window.title('AstralJaeger\' path editor v1.0.0')
+def add_entry():
+    pass
+
+
+def remove_entry():
+    global treeview
+    selected = treeview.focus()
+    if not selected:
+        return
+    treeview.delete(selected)
+    applications.remove(treeview.item(selected)['values'][0])
+    update_statistics()
+
+
+def up_entry():
+    global treeview
+    selected = treeview.focus()
+    index = treeview.index(selected)
+    if index == 0:
+        return
+    treeview.move(selected, treeview.parent(selected), index - 1)
+    applications[index], applications[index - 1] = applications[index - 1], applications[index]
+
+
+def down_entry():
+    global treeview
+    selected = treeview.focus()
+    index = treeview.index(selected)
+    if index == len(treeview.get_children()) - 1:
+        return
+    treeview.move(selected, treeview.parent(selected), index + 1)
+    applications[index], applications[index + 1] = applications[index + 1], applications[index]
+
+
+def reload_path():
+    global treeview
+    treeview.delete(*treeview.get_children())
+    for app in get_path_from_os():
+        exists = path.exists(app)
+        filecount = (get_filecount(app) if exists else 0)
+        system = 'windows/system32' in app
+        data = (app, filecount)
+        treeview.insert('', tk.END, values=data, tags=[
+            ('exists' if exists else 'nexists'),
+            ('sys32' if system else 'nsys32')
+        ])
+    update_statistics()
+
+
+def save_path():
+    pass
+
+
+def remove_duplicates():
+    global applications, treeview
+    duplicates: list[str] = [item for item, count in collections.Counter(applications).items() if count > 1]
+    for index, child in enumerate(reversed(treeview.get_children())):
+        item = treeview.item(child)
+        value = item['values'][0]
+        if value in duplicates:
+            duplicates.remove(value)
+            treeview.delete(child)
+            applications.remove(value)
+    update_statistics()
+
+
+def remove_dead():
+    global treeview
+    for child in treeview.get_children(''):
+        item = treeview.item(child)
+        if 'nexists' in item['tags']:
+            treeview.delete(child)
+            applications.remove(item['values'][0])
+    update_statistics()
+
+
+def create_widgets(window: Tk) -> None:
+    window.title('AstralJaeger\'s path editor v1.0.0')
     window.geometry('640x480')
     window.grid_columnconfigure(0, weight=2)
     window.grid_columnconfigure(2, weight=1)
-    columns = ('path', )
+    columns = ('path', 'filecount')
 
-    table = ttk.Treeview(window, columns=columns, show='headings')
-    table.heading('path', text='Path')
+    global treeview, duplicates_label, total_entries_label, total_length_label
+    treeview = ttk.Treeview(window, columns=columns, show='headings', selectmode='browse')
+    treeview.heading('path', text='Path')
+    treeview.heading('filecount', text='Filecount')
 
-    for app in applications:
-        t = (app, )
-        table.insert('', tk.END, values=t, tags=('exists' if path.exists(app) else 'nexists'))
+    for app in get_path_from_os():
+        exists = path.exists(app)
+        filecount = (get_filecount(app) if exists else 0)
+        system = 'windows/system32' in app
+        t = (app, filecount)
+        treeview.insert('', tk.END, values=t, tags=[
+            ('exists' if exists else 'nexists'),
+            ('sys32' if system else 'nsys32')
+        ])
 
-    table.tag_configure('exists', background='white')
-    table.tag_configure('nexists', background='orange')
-    # table.bind('<<TreeviewSelect>>', item_selected)
+    treeview.tag_configure('nexists', background='orange')
+    treeview.tag_configure('sys32', foreground='gray')
 
-    table.place(relx=0.05, rely=0.05, relwidth=0.75, relheight=0.75)
+    treeview.place(relx=0.05, rely=0.05, relwidth=0.75, relheight=0.75)
 
-    scrollbar = Scrollbar(window, orient=tk.VERTICAL, command=table.yview)
-    table.configure(yscroll=scrollbar.set)
+    scrollbar = Scrollbar(window, orient=tk.VERTICAL, command=treeview.yview)
+    treeview.configure(yscroll=scrollbar.set)
     scrollbar.place(relx=0.8, rely=0.05, relwidth=0.025, relheight=0.75)
 
     # Duplicates label
@@ -66,40 +154,45 @@ def create_widgets(window: Tk, applications) -> None:
     duplicates_label.place(relx=0.05, rely=0.825, relwidth=0.75, relheight=0.05)
 
     # Remove duplicates
-    remove_duplicates = Button(text='RM Duplicate')
-    remove_duplicates.place(relx=0.85, rely=0.825, relwidth=0.125, relheight=0.05)
+    remove_duplicates_btn = Button(text='RM Duplicate', command=remove_duplicates)
+    remove_duplicates_btn.place(relx=0.85, rely=0.825, relwidth=0.125, relheight=0.05)
 
     # Remove non-existent entries
-    remove_nonexistent = Button(text='RM Dead')
-    remove_nonexistent.place(relx=0.85, rely=0.885, relwidth=0.125, relheight=0.05)
+    remove_nonexistent_btn = Button(text='RM Dead', command=remove_dead)
+    remove_nonexistent_btn.place(relx=0.85, rely=0.885, relwidth=0.125, relheight=0.05)
 
     # Total count label
-    total_label = Label(text=f'Total entries: {len(applications)}')
-    total_label.place(relx=0.05, rely=0.885, relwidth=0.75, relheight=0.05)
+    total_entries_label = Label(text=f'Total entries: {len(applications)}')
+    total_entries_label.place(relx=0.05, rely=0.885, relwidth=0.75, relheight=0.05)
+
+    # Total length label
+    total_length_label = Label(text=f'Total Length: {get_path_length(applications)}')
+    total_length_label.place(relx=0.05, rely=0.945, relwidth=0.75, relheight=0.05)
 
     # Add entry
-    add_entry = Button(text='Add')
-    add_entry.place(relx=0.85, rely=0.05, relwidth=0.125, relheight=0.05)
+    add_entry_btn = Button(text='Add', command=add_entry)
+    add_entry_btn.place(relx=0.85, rely=0.05, relwidth=0.125, relheight=0.05)
 
     # Remove entry
-    remove_entry = Button(text='Remove')
-    remove_entry.place(relx=0.85, rely=0.11, relwidth=0.125, relheight=0.05)
+    remove_entry_btn = Button(text='Remove', command=remove_entry)
+    remove_entry_btn.place(relx=0.85, rely=0.11, relwidth=0.125, relheight=0.05)
 
     # Move entry up
-    up = Button(text='Up')
-    up.place(relx=0.85, rely=0.17, relwidth=0.125, relheight=0.05)
+    up_btn = Button(text='Up', command=up_entry)
+    up_btn.place(relx=0.85, rely=0.17, relwidth=0.125, relheight=0.05)
 
     # Move entry down
-    down = Button(text='Down')
-    down.place(relx=0.85, rely=0.23, relwidth=0.125, relheight=0.05)
+    down_btn = Button(text='Down', command=down_entry)
+    down_btn.place(relx=0.85, rely=0.23, relwidth=0.125, relheight=0.05)
 
     # Reload path
-    reload = Button(text='Reload')
-    reload.place(relx=0.85, rely=0.45, relwidth=0.125, relheight=0.05)
+    reload_btn = Button(text='Reload', command=reload_path)
+    reload_btn.place(relx=0.85, rely=0.45, relwidth=0.125, relheight=0.05)
 
     # Save path
-    save = Button(text='Save')
-    save.place(relx=0.85, rely=0.51, relwidth=0.125, relheight=0.05)
+    save_btn = Button(text='Save', command=save_path)
+    save_btn.place(relx=0.85, rely=0.51, relwidth=0.125, relheight=0.05)
+    update_statistics()
 
 
 def item_selected(event):
@@ -108,6 +201,41 @@ def item_selected(event):
 
 def destroy_window(window: tk.Tk):
     window.destroy()
+
+
+def update_statistics():
+    update_duplicate_count()
+    update_total_entries_count()
+    update_total_length()
+
+
+def update_duplicate_count():
+    global applications, duplicates_label
+    duplicates_label['text'] = f'Duplicates found: {get_duplicate_count(applications)}'
+
+
+def update_total_entries_count():
+    global applications, total_entries_label
+    total_entries_label['text'] = f'Total entries: {len(applications)}'
+
+
+def update_total_length():
+    global applications, total_length_label
+    total_length_label['text'] = f'Total length: {get_path_length(applications)}'
+
+
+def get_duplicate_count(apps: List[str]) -> int:
+    unique_apps = set(apps)
+    duplicates = len(apps) - len(unique_apps)
+    return duplicates
+
+
+def get_filecount(dir: str):
+    return len([name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))])
+
+
+def get_path_length(apps: List[str]) -> int:
+    return sum(map(lambda app: len(app), apps))
 
 
 def get_path_from_os() -> List[str]:
@@ -119,15 +247,15 @@ def get_path_from_os() -> List[str]:
     env_path = env_path.replace('\\r\\n', '')
     env_path = env_path.replace('\\\\', '/')
     env_path = env_path.lower()
-    applications: List[str] = env_path.split(';')
+    global applications
+    apps: List[str] = env_path.split(';')
+
+    applications = apps
     return applications
 
 
-def get_duplicate_count(applications: List[str]) -> int:
-    unique_applications = set(applications)
-    duplicates = len(applications) - len(unique_applications)
-    # print(f'Duplicates found: {duplicates}')
-    return duplicates
+def run_command(command: str) -> Union[CompletedProcess, CompletedProcess[bytes]]:
+    return subprocess.run(['powershell.exe', command], capture_output=True)
 
 
 def is_admin():
@@ -135,10 +263,6 @@ def is_admin():
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-
-
-def run_command(command: str) -> Union[CompletedProcess, CompletedProcess[bytes]]:
-    return subprocess.run(['powershell.exe', command], capture_output=True)
 
 
 if __name__ == '__main__':
