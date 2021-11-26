c8f30420706baf14d4ee1d923d9d72a344b48170
Felix Hillebrand <97hilfel@gmail.com>
2021-11-26 12:05:37 +0100

    Implement adding and editing of entries.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import sys
 import subprocess
 import tkinter as tk
-import tkinter.ttk
 import collections
 
 from typing import List, Union, Tuple
@@ -12,8 +11,10 @@ from subprocess import CompletedProcess
 from tkinter import *
 from tkinter import ttk
 
+
 debug = True
 applications: List[str] = []
+selected: str = ''
 treeview: ttk.Treeview
 duplicates_label: Label
 total_entries_label: Label
@@ -34,12 +35,107 @@ def create_window() -> Tk:
     return Tk()
 
 
+add_text: Text
+add_error_label: Label
+add_popup: tk.Toplevel
+
+
 def add_entry():
-    pass
+    global add_popup
+    add_popup = tk.Toplevel()
+    add_popup.wm_title('Add item')
+
+    label = Label(add_popup, text='Path:')
+    label.grid(column=0, row=0)
+
+    global add_text, add_error_label
+    add_text = Text(add_popup, height=1)
+    add_text.grid(column=1, row=0, rowspan=2)
+
+    add_error_label = Label(add_popup)
+    add_error_label.grid(column=0, row=1, rowspan=4)
+
+    button = Button(add_popup, text='OK', command=confirm_add)
+    button.grid(column=3, row=0)
+
+
+def confirm_add():
+    global add_text, add_error_label, applications, treeview
+
+    filepath: str = add_text.get(1.0, "end-1c")
+    if not path.exists(filepath):
+        add_error_label['text'] = 'Path does not exist!'
+        return
+    else:
+        add_error_label['text'] = ''
+
+    filepath = str(filepath)
+    filepath = filepath.replace('\\r\\n', '')
+    filepath = filepath.replace('\\\\', '/')
+    filepath = filepath.replace('\\', '/')
+    filepath = filepath.lower()
+
+    filecount = (get_filecount(filepath) if path.exists(filepath) else 0)
+    data = (filepath, filecount)
+    treeview.insert('', tk.END, values=data, tags=('sys32' if 'windows/system32' in filepath else 'nsys32'))
+    applications.append(filepath)
+    add_popup.destroy()
+
+
+edit_text: Text
+edit_error_label: Label
+edit_popup: tk.Toplevel
+edit_old_path: str = ''
 
 
 def edit_entry():
-    pass
+    global edit_popup, edit_old_path, selected
+    edit_popup = tk.Toplevel()
+    edit_popup.wm_title('Edit item')
+
+    label = Label(edit_popup, text='Path:')
+    label.grid(column=0, row=0)
+
+    global edit_text, edit_error_label
+    edit_text = Text(edit_popup, height=1)
+    edit_text.grid(column=1, row=0, rowspan=2)
+    edit_text.insert(1.0, selected)
+    edit_old_path = selected
+
+    edit_error_label = Label(edit_popup)
+    edit_error_label.grid(column=0, row=1, rowspan=4)
+
+    button = Button(edit_popup, text='OK', command=confirm_edit)
+    button.grid(column=3, row=0)
+
+
+def confirm_edit():
+    global edit_text, edit_old_path, edit_error_label, edit_popup, applications, treeview
+
+    filepath: str = edit_text.get(1.0, "end-1c")
+    if not path.exists(filepath):
+        edit_error_label['text'] = 'Path does not exist!'
+        return
+    else:
+        edit_error_label['text'] = ''
+
+    filepath = str(filepath)
+    filepath = filepath.replace('\\r\\n', '')
+    filepath = filepath.replace('\\\\', '/')
+    filepath = filepath.replace('\\', '/')
+    filepath = filepath.lower()
+
+    filecount = (get_filecount(filepath) if path.exists(filepath) else 0)
+    data = (filepath, filecount)
+
+    old_item = treeview.item(treeview.focus())['values'][0]
+    treeview.delete(treeview.focus())
+    treeview.insert('', tk.END, values=data, tags=('sys32' if 'windows/system32' in filepath else 'nsys32'))
+    applications.append(filepath)
+    applications.remove(old_item)
+
+    update_statistics()
+    edit_popup.destroy()
 
 
 def remove_entry():
@@ -47,8 +143,8 @@ def remove_entry():
     selected = treeview.focus()
     if not selected:
         return
-    treeview.delete(selected)
     applications.remove(treeview.item(selected)['values'][0])
+    treeview.delete(selected)
     update_statistics()
 
 
@@ -82,13 +178,15 @@ def reload_path():
         data = (app, filecount)
         treeview.insert('', tk.END, values=data, tags=[
             ('exists' if exists else 'nexists'),
-            ('sys32' if system else 'nsys32')
+            ('sys32' if system else 'nsys32'),
+            ('empty' if filecount == 0 else 'nempty')
         ])
     update_statistics()
 
 
 def save_path():
-    pass
+    global applications
+    set_path_to_os(applications)
 
 
 def remove_duplicates():
@@ -127,6 +225,7 @@ def create_widgets(window: Tk) -> None:
     treeview.heading('filecount', text='Filecount')
 
     treeview.tag_configure('nexists', background='orange')
+    treeview.tag_configure('empty', background='lightblue')
     treeview.tag_configure('sys32', foreground='gray')
 
     treeview.place(relx=0.05, rely=0.05, relwidth=0.75, relheight=0.75)
@@ -134,6 +233,8 @@ def create_widgets(window: Tk) -> None:
     scrollbar = Scrollbar(window, orient=tk.VERTICAL, command=treeview.yview)
     treeview.configure(yscroll=scrollbar.set)
     scrollbar.place(relx=0.8, rely=0.05, relwidth=0.025, relheight=0.75)
+
+    treeview.bind('<ButtonRelease-1>', item_selected)
 
     # Duplicates label
     duplicates_label = Label(text=f'Duplicates: {get_duplicate_count(applications)}')
@@ -186,11 +287,15 @@ def create_widgets(window: Tk) -> None:
     save_btn = Button(text='Save', command=save_path)
     save_btn.place(relx=0.85, rely=0.55, relwidth=0.125, relheight=0.05)
 
+    # Load path-variable from os
     reload_path()
 
 
 def item_selected(event):
-    return
+    global selected, treeview
+    sel_item = treeview.item(treeview.focus())
+    if hasattr(sel_item, 'values') and len(sel_item['values']) >= 1:
+        selected = sel_item['values'][0]
 
 
 def destroy_window(window: tk.Tk):
@@ -248,6 +353,17 @@ def get_path_from_os() -> List[str]:
     return applications
 
 
+def set_path_to_os(path: List[str]) -> None:
+
+    if is_admin():
+        command = f'[Environment]::SetEnvironmentVariable("Path", \'{";".join(path)}\', [System.EnvironmentVariableTarget]::Machine)'
+    else:
+        command = f'[Environment]::SetEnvironmentVariable("Path", \'{";".join(path)}\', [System.EnvironmentVariableTarget]::User)'
+    print(command)
+    if not debug:
+        run_command(command)
+
+
 def run_command(command: str) -> Union[CompletedProcess, CompletedProcess[bytes]]:
     return subprocess.run(['powershell.exe', command], capture_output=True)
 
@@ -260,7 +376,4 @@ def is_admin():
 
 
 if __name__ == '__main__':
-    if debug or is_admin():
-        main()
-    else:
-        ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, ' '.join(sys.argv), None, 1)
+    main()
